eb3ce9b03c8c5e222460401b4bff21150af8efc3
Ecconia <public-commit@ecconia.com>
2023-05-16 16:32:04 +0200

    Obtain game/output folder from arguments
    Instead of caching and querying them.
    This allows easy non-interactive command line usage.
    
    Also changed some variable names. And switched to 'using', which changes indentation.

--- LwDecomp/Program.cs
+++ LwDecomp/Program.cs
@@ -3,88 +3,55 @@ using ICSharpCode.Decompiler.CSharp;
 using ICSharpCode.Decompiler.CSharp.ProjectDecompiler;
 using ICSharpCode.Decompiler.Metadata;
 using System.Diagnostics;
-using System.Reflection;
 
 namespace LwDecomp
 {
-	internal class Program
+	internal static class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
-			var ourFolder = new FileInfo(Assembly.GetExecutingAssembly().Location).DirectoryName!;
-			string LWPathCacheFile = Path.Combine(ourFolder, ".lwInstallPath");
-			
-			Console.WriteLine("Checking Logicworld install path...");
-			var lwInstallPath = File.Exists(LWPathCacheFile) ? File.ReadAllText(LWPathCacheFile) : null;
-			
-			bool pathChanged = false;
-			bool pathok = false;
-			do
+			if(args.Length != 2)
 			{
-				if (string.IsNullOrEmpty(lwInstallPath))
-				{
-					Console.WriteLine("Looks like the Logicworld install path hasnt been set yet.");
-				}
-				else if (!Directory.Exists(lwInstallPath))
-				{
-					Console.WriteLine("Looks like the cached path no longer exists.");
-				}
-				else if (!Directory.Exists(Path.Combine(lwInstallPath, "Logic_World_Data")))
-				{
-					Console.WriteLine("The Logic_World_Data folder cannot be found inside the given install dir");
-				}
-				else if (!Directory.Exists(Path.Combine(lwInstallPath, "Server")))
-				{
-					Console.WriteLine("The Server folder cannot be found inside the given install dir");
-				}
-				else
-				{
-					pathok = true;
-				}
-				
-				if (!pathok)
-				{
-					Console.WriteLine("Please enter the path to the topmost Logicworld directory and then press ENTER. (The folder that contains the Logic_World.exe file along with subfolders such as Logic_World_Data):");
-					lwInstallPath = Console.ReadLine()?.Trim() ?? "";
-					pathChanged = true;
-				}
-			} while (!pathok);
-
-			if(pathChanged)
-			{
-				File.WriteAllText(LWPathCacheFile, lwInstallPath);
+				exit("Expected two arguments: <game-directory> <output-directory>");
 			}
-
-			Console.WriteLine("Install path OK.");
-			
-			Console.WriteLine("Do you want to decompile the LW-server and LW-client dlls? [Y/n]");
-			
-			var input = Console.ReadLine()?.Trim()?.ToLowerInvariant() ?? "n";
-			if (input != "" && input != "y")
+			var gameDirectory = args[0];
+			if(!Directory.Exists(gameDirectory))
 			{
-				Console.WriteLine("Decompilation aborted by user.");
-				return;
+				exit("Game directory does not exist.");
 			}
+			var outputDirectory = args[1];
+			if(!Directory.Exists(outputDirectory))
+			{
+				exit("Game directory does not exist.");
+			}
+			
+			var clientFolder = Path.Combine(gameDirectory, "Logic_World_Data", "Managed");
+			if(!Directory.Exists(clientFolder))
+			{
+				exit("Client DLL folder missing.");
+			}
+			var serverFolder = Path.Combine(gameDirectory, "Server");
+			if(!Directory.Exists(serverFolder))
+			{
+				exit("Server DLL folder missing.");
+			}
+			
+			//Start decompilation:
 			
 			var skipPrefixes = "Microsoft.;System.;UnityEngine.;Unity.;mscorlib.;netstandard.;WindowsBase.;Newtonsoft.Json.Unity.".Split(";");
-			var serverSubpath = "Server";
-			var clientSubpath = Path.Combine("Logic_World_Data", "Managed");
 			
 			Console.WriteLine("\nDecompiling server...");
-			
-			var decompRootFolder = Path.Combine(ourFolder, "decompiled");
-			
 			DecompDirectory(
-				Path.Combine(decompRootFolder, "LWServer"), 
-				Path.Combine(lwInstallPath, serverSubpath), 
-				files => files.Where(f => f.EndsWith(".dll") && !skipPrefixes.Any(keyword => f.StartsWith(keyword)))
+				Path.Combine(outputDirectory, "server"), 
+				serverFolder, 
+				files => files.Where(f => f.EndsWith(".dll") && !skipPrefixes.Any(f.StartsWith))
 			);
 			
 			Console.WriteLine("\nDecompiling client...");
 			DecompDirectory(
-				Path.Combine(decompRootFolder, "LWClient"), 
-				Path.Combine(lwInstallPath, clientSubpath), 
-				files => files.Where(f => f.EndsWith(".dll") && !skipPrefixes.Any(keyword => f.StartsWith(keyword)))
+				Path.Combine(outputDirectory, "client"), 
+				clientFolder, 
+				files => files.Where(f => f.EndsWith(".dll") && !skipPrefixes.Any(f.StartsWith))
 			);
 			
 			Console.WriteLine("Finished successfully.");
@@ -92,8 +59,8 @@ namespace LwDecomp
 		
 		private static void DecompDirectory(string outputFolder, string dllSourceFolder, Func<string[], IEnumerable<string>> decompFileNameSelector)
 		{
-			var totalSw = new Stopwatch();
-			totalSw.Start();
+			var totalRuntime = new Stopwatch();
+			totalRuntime.Start();
 
 			if(Directory.Exists(outputFolder))
 			{
@@ -109,46 +76,49 @@ namespace LwDecomp
 			                    .Where(x => x.EndsWith(".dll"))
 			                    .Select(x => Path.Combine(dllSourceFolder, x));
 			
-			var ds = new DecompilerSettings(LanguageVersion.Latest)
-			{
-			};
-			var decSw = new Stopwatch();
+			var ds = new DecompilerSettings(LanguageVersion.Latest);
+			var entryRuntime = new Stopwatch();
 			foreach (var f in filesToDecomp)
 			{
-				decSw.Restart();
-				var decDllName = f.Split(Path.DirectorySeparatorChar).Last();
+				entryRuntime.Restart();
 				
-				Console.Write($"Decompiling {decDllName}... ");
+				var decompiledDllName = f.Split(Path.DirectorySeparatorChar).Last();
+				Console.Write($"Decompiling {decompiledDllName}... ");
 				
-				var decProjName = decDllName.Replace(".dll", "") + ".csproj";
+				var decProjName = decompiledDllName.Replace(".dll", "") + ".csproj";
 				
 				var module = new PEFile(f);
 				var assemblyResolver = new UniversalAssemblyResolver(f, true, module.DetectTargetFrameworkId());
 				var cd = new WholeProjectDecompiler(ds, assemblyResolver, assemblyResolver, null); // maybe add PDB provider
 				
-				var decOutputFolder = Path.Combine(outputFolder, decDllName);
+				var decOutputFolder = Path.Combine(outputFolder, decompiledDllName);
 				Directory.CreateDirectory(decOutputFolder);
 				var projectFileName = Path.Combine(decOutputFolder, decProjName);
-				using (var projectFileWriter = new StreamWriter(File.OpenWrite(projectFileName)))
+				using var projectFileWriter = new StreamWriter(File.OpenWrite(projectFileName));
+				try
 				{
-					try
-					{
-						var decProjectId = cd.DecompileProject(module, Path.GetDirectoryName(projectFileName), projectFileWriter);
-						decSw.Stop();
-					}
-					catch(Exception e)
-					{
-						Console.WriteLine("error!");
-						Console.WriteLine("\nError: " + e.Message + "\n");
-					}
-					finally
-					{
-						Console.WriteLine($"done in {decSw.ElapsedMilliseconds}ms!");
-					}
+					cd.DecompileProject(module, Path.GetDirectoryName(projectFileName), projectFileWriter);
+					entryRuntime.Stop();
+				}
+				catch(Exception e)
+				{
+					Console.WriteLine("error!");
+					Console.WriteLine("\nError: " + e.Message + "\n");
+				}
+				finally
+				{
+					Console.WriteLine($"done in {entryRuntime.ElapsedMilliseconds}ms!");
 				}
 			}
-			totalSw.Stop();
-			Console.WriteLine($"Decompilation done in {totalSw.ElapsedMilliseconds}ms!");
+			
+			totalRuntime.Stop();
+			Console.WriteLine($"Decompilation done in {totalRuntime.ElapsedMilliseconds}ms!");
+		}
+		
+		private static void exit(string message)
+		{
+			Console.WriteLine(message);
+			Environment.Exit(1);
 		}
 	}
 }
